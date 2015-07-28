f396a046baab4fa068c39f3dfb056c55382f2baa
Stefan <dev@2904.cc>
2015-07-28 20:16:58 +0200

    added timeout to sync

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tail -n 1 temp_log.csv | ssh home "cat >> /var/www/camp/temp/temp_log.csv"
+tail -n 1 temp_log.csv | ssh -o ConnectTimeout=30 home "cat >> /var/www/camp/temp/temp_log.csv"
