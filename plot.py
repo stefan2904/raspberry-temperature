8483865b4a5c7e36a4112a5dc6a46fc30e7a8e65
stefan <dev@2904.cc>
2015-07-28 02:35:28 +0200

    implemented plotting of temperature

--- plot.py
+++ plot.py
@@ -3,6 +3,8 @@ import matplotlib
 matplotlib.use('Agg')
 
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.ticker as tkr
 import datetime
 import numpy as np
 import csv
@@ -11,6 +13,8 @@ import csv
 #y = np.random.randint(100, size=x.shape)
 
 data = {}
+
+# read data:
 
 with open('temp_log.csv', 'rb') as csvfile:
     logreader = csv.DictReader(csvfile, delimiter=';',quotechar='|', quoting=csv.QUOTE_MINIMAL)
@@ -21,12 +25,28 @@ with open('temp_log.csv', 'rb') as csvfile:
             except KeyError:
                 data[header] = [value]
 
+# prepare data:
 
 x = map(lambda x: datetime.datetime.fromtimestamp(int(x)), data['time'])
 y = data['temp']
 
+# actual plotting:
+
+def xfmt(x,pos=None):
+    ''' custom date formatting '''
+    x = mdates.num2date(x)
+    label = x.strftime('%m/%d')
+    label = label.lstrip('0')
+    return label
+
 
 plt.plot(x,y)
-#plt.show()
+
+plt.setp(plt.gca().xaxis.get_majorticklabels(),rotation=90)
+plt.gca().xaxis.set_major_formatter(tkr.FuncFormatter(xfmt))
+plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=4))
+plt.gca().xaxis.set_minor_locator(mdates.DayLocator())
+
+plt.show()
 
 plt.savefig('temp_plot.png')
