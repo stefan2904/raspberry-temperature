ac50c351e1394e5d75fdeff9f10dfc020dbd0066
r3pi <dev+r3pi@2904.cc>
2015-07-31 03:35:05 +0200

    fixed bug in r3 door helper

--- r3door.py
+++ r3door.py
@@ -5,6 +5,6 @@ def getDoorstatus():
     resp = requests.get(url="http://realraum.at/status.json")
     data = json.loads(resp.text)
     locked = data['sensors']['door_locked'][0]['value']
-    kontakted = data['sensors']['door_locked'][0]['value']
+    kontakted = data['sensors']['door_locked'][1]['value']
     return (locked, kontakted)
 
